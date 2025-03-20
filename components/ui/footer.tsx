02e65965cb3a2278e90f8f7fc3f6b621295627a8
Rafael M Mudafort <rafmudaf@gmail.com>
2025-03-20 15:27:39 -0500

    Add custom spinner logo

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -11,9 +11,9 @@ export default function Footer({ border = false }: { border?: boolean }) {
         >
           {/* 1st block */}
           <div className="space-y-2 sm:col-span-12 lg:col-span-4">
-            <div>
+            {/* <div>
               <Logo />
-            </div>
+            </div> */}
             <div className="text-sm text-gray-600">
               &copy; Cruip.com - All rights reserved.
             </div>
