c63096df7ba4ac0f436f8db00edb519a3578a9cc
Rafael M Mudafort <rafmudaf@gmail.com>
2025-03-20 15:43:51 -0500

    Add build workflow

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "WTG",
+  description: "Wind Technology Group",
 };
 
 import Hero from "@/components/hero-home";
--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    /**
+     * Enable static exports.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
+
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    // basePath: "/windtechnologygroup.github.io",
+
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: 'dist',
+}
 
 module.exports = nextConfig;
