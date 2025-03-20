dc270767a5291bd42c9f5ee7ec724fd14dce8a41
Rafael M Mudafort <rafmudaf@gmail.com>
2025-03-20 15:25:29 -0500

    Replace some avatars

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
 import PageIllustration from "@/components/page-illustration";
-import Avatar01 from "@/public/images/avatar-01.jpg";
+import AvatarRaf from "@/public/images/avatar-raf.jpg";
 import Avatar02 from "@/public/images/avatar-02.jpg";
 import Avatar03 from "@/public/images/avatar-03.jpg";
-import Avatar04 from "@/public/images/avatar-04.jpg";
-import Avatar05 from "@/public/images/avatar-05.jpg";
-import Avatar06 from "@/public/images/avatar-06.jpg";
 
 export default function HeroHome() {
   return (
@@ -21,9 +18,9 @@ export default function HeroHome() {
               data-aos="zoom-y-out"
             >
               <div className="-mx-0.5 flex justify-center -space-x-3">
-                <Image
+                {/* <Image
                   className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar01}
+                  src={AvatarRaf}
                   width={32}
                   height={32}
                   alt="Avatar 01"
@@ -41,28 +38,7 @@ export default function HeroHome() {
                   width={32}
                   height={32}
                   alt="Avatar 02"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar04}
-                  width={32}
-                  height={32}
-                  alt="Avatar 03"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar05}
-                  width={32}
-                  height={32}
-                  alt="Avatar 04"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar06}
-                  width={32}
-                  height={32}
-                  alt="Avatar 05"
-                />
+                /> */}
               </div>
             </div>
             <h1
